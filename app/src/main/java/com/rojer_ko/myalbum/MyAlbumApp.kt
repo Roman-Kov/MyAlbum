d6a40fb2099de98efe1179edd3afa65aaaf86e15
Roman-Kov <rojer_ko@bk.ru>
2021-03-06 21:45:09 +0300

    1. Создал класс приложения "MyAlbumApp" 2. Добавил имя приложения в манифест, прописал зависимости для Интернета

---
+++ app/src/main/java/com/rojer_ko/myalbum/MyAlbumApp.kt
@@ -0,0 +1,17 @@
+package com.rojer_ko.myalbum
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyAlbumApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MyAlbumApp)
+            modules(listOf())
+        }
+    }
+}
