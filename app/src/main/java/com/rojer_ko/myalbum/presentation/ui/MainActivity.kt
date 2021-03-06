c70d2732dd5114396e22298a5ec5ef329d2bdd6d
Roman-Kov <rojer_ko@bk.ru>
2021-03-06 22:27:42 +0300

    1. Создал пустые фрагменты 2. Добавил цвета, размеры, изменил тему приложения 3. Создал граф навигации и меню для bottom navigation 4. Настроил на MainActivity навигацию

--- app/src/main/java/com/rojer_ko/myalbum/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/rojer_ko/myalbum/presentation/ui/MainActivity.kt
@@ -2,12 +2,22 @@ package com.rojer_ko.myalbum.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.rojer_ko.myalbum.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavController()
+    }
+
+    private fun initNavController() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        main_bottom_navigation.setupWithNavController(navController)
     }
 }
